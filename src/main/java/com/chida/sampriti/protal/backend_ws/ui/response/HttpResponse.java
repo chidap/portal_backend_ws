32263a0eb5232985304d3a2557ccf7356634d02f
Chida <chida_p@yahoo.com>
2021-09-24 14:02:01 +0530

    Updated with HttpResponse for sending response back to the client side

---
+++ src/main/java/com/chida/sampriti/protal/backend_ws/ui/response/HttpResponse.java
@@ -0,0 +1,49 @@
+package com.chida.sampriti.protal.backend_ws.ui.response;
+
+public class HttpResponse {
+    private int httpStatusCode;
+    private String httpStatus;
+    private String reason;
+    private String message;
+
+    public HttpResponse() {}
+    
+    public HttpResponse(int httpStatusCode, String httpStatus, String reason, String message) {
+        this.httpStatusCode = httpStatusCode;
+        this.httpStatus = httpStatus;
+        this.reason = reason;
+        this.message = message;
+    }
+
+    public int getHttpStatusCode() {
+        return httpStatusCode;
+    }
+
+    public void setHttpStatusCode(int httpStatusCode) {
+        this.httpStatusCode = httpStatusCode;
+    }
+
+    public String getHttpStatus() {
+        return httpStatus;
+    }
+
+    public void setHttpStatus(String httpStatus) {
+        this.httpStatus = httpStatus;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
