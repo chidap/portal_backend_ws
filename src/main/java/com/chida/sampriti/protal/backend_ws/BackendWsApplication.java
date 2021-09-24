a7271c729f1c50ff8087058bc5b4d5b8227523b0
Chida <chida_p@yahoo.com>
2021-09-24 23:22:32 +0530

    Modified application.properties to application.yml further

--- src/main/java/com/chida/sampriti/protal/backend_ws/BackendWsApplication.java
+++ src/main/java/com/chida/sampriti/protal/backend_ws/BackendWsApplication.java
@@ -2,12 +2,17 @@ package com.chida.sampriti.protal.backend_ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.chida.sampriti.protal.backend_ws"})
 public class BackendWsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendWsApplication.class, args);
 	}
+
 
 }
