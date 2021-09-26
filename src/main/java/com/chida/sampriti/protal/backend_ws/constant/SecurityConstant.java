63954600213271c6b6ffe23c4e4c67e75c4929b3
Chida <chida_p@yahoo.com>
2021-09-26 11:08:38 +0530

    Updated with UserController and SecurityConstant class

---
+++ src/main/java/com/chida/sampriti/protal/backend_ws/constant/SecurityConstant.java
@@ -0,0 +1,15 @@
+package com.chida.sampriti.protal.backend_ws.constant;
+
+public class SecurityConstant {
+    public static final long EXPIRATION_TIME = 86_400_000; // 1 day in milliseconds
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String JWT_TOKEN_HEADER = "Jwt-Token";
+    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token can not be verified";
+    public static final String GET_ARRAYS_LLC = "Get Arrays, LLC";
+    public static final String GET_ARRAYS_ADMINISTRATION = "User Management Portal";
+    public static final String AUTHORITIES = "authorities";
+    public static final String FORBIDDEN_MESSAGE = "You need to login to access this page";
+    public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
+    public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
+    public static final String[] PUBLIC_URLS = { "/user/login", "/user/register", "/user/resetpassword/**", "/user/image/**"};
+}
