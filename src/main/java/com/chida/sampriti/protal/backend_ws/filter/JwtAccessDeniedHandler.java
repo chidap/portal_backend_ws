bed49215d671aa0756e10f9e9f0a3ccfb30a7d6d
Chida <chida_p@yahoo.com>
2021-09-28 22:33:57 +0530

    Updated with Access Denied Handler

--- src/main/java/com/chida/sampriti/protal/backend_ws/filter/JwtAccessDeniedHandler.java
+++ src/main/java/com/chida/sampriti/protal/backend_ws/filter/JwtAccessDeniedHandler.java
@@ -3,27 +3,28 @@ package com.chida.sampriti.protal.backend_ws.filter;
 import com.chida.sampriti.protal.backend_ws.constant.SecurityConstant;
 import com.chida.sampriti.protal.backend_ws.ui.response.HttpResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Locale;
 
-import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException exception) throws IOException, ServletException {
         HttpResponse httpResponse = new HttpResponse(
-                FORBIDDEN.value(), FORBIDDEN, FORBIDDEN.getReasonPhrase().toUpperCase(), SecurityConstant.FORBIDDEN_MESSAGE);
+                UNAUTHORIZED.value(), UNAUTHORIZED, UNAUTHORIZED.getReasonPhrase().toUpperCase(), SecurityConstant.ACCESS_DENIED_MESSAGE);
         response.setContentType(APPLICATION_JSON_VALUE);
-        response.setStatus(FORBIDDEN.value());
+        response.setStatus(UNAUTHORIZED.value());
         OutputStream outputStream = response.getOutputStream();
         ObjectMapper mapper = new ObjectMapper();
         mapper.writeValue(outputStream, httpResponse);
